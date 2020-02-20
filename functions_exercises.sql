184cccf4c071b1b11ea24f6dc14563faa441be02
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-02-20 11:36:00 -0600

    MySQL functions exercise done

---
+++ functions_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+SELECT * FROM employees
+WHERE gender = 'M'
+  AND (
+            first_name IN ('Irena', 'Vidya')
+        OR first_name = 'Maya'
+    )
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT CONCAT(first_name, ' ' ,last_name) AS Result FROM employees.employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+
+SELECT CONCAT(first_name, ' ', last_name, ' has worked ',
+       DATEDIFF(curdate(), hire_date), ' days here.') AS Results
+       FROM employees
+       WHERE hire_date > '1990' and hire_date < '2000'
+         AND birth_date LIKE '%12-25%'
+       ORDER BY birth_date ASC ,hire_date DESC;
+
+
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+
